f2e131f20ebdcbacf138329235ff95f965de707e
RIZKA    ----- Anime Lovers ----- <116262828+rizkaalt@users.noreply.github.com>
2023-06-19 08:54:46 +0000

    Update search.js

--- js/search.js
+++ js/search.js
@@ -11,7 +11,7 @@ let showingResultsForDisplayed = false;
 const errorContainer = document.createElement("div");
 errorContainer.style.color = "red";
 
-fetch('https://api.consumet.org/anime/gogoanime/' + query)
+fetch('https://rizka-english-anime-api.vercel.app/anime/zoro/' + query)
     .then(response => response.json())
     .then(data => {
         var cardDiv = document.getElementById("card");
